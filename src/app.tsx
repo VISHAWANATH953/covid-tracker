c33aa9209fa9f4d4d5fcc0ff6c62cf7b2828b5dd
Vishawanath kokare <vishkokre@gmail.com>
2021-05-14 19:30:33 +0530

    DONE

---
+++ src/app.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "preact/hooks";
+export const App = () => {
+  const [data, setData] = useState<any>([]);
+  const getData = async () => {
+    const res = await fetch("https://api.covid19india.org/data.json");
+    const dat = await res.json();
+    setData(dat.statewise[0]);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <main>
+      <h1>Covid Tracker Live</h1>
+      <div className="boxdiv-boxes">
+        <div className="boxdiv-boxes-box">
+          <h2>
+            OUR<span>COUNTRY</span>
+          </h2>
+          <p>INDIA</p>
+        </div>
+        <div className="boxdiv-boxes-box">
+          <h2>
+            TOTAL<span>RECOVERED</span>
+          </h2>
+          <p>{data.recovered}</p>
+        </div>
+        <div className="boxdiv-boxes-box">
+          <h2>
+            TOTAL<span>CONFIRMED</span>
+          </h2>
+          <p>{data.confirmed}</p>
+        </div>
+        <div className="boxdiv-boxes-box">
+          <h2>
+            TOTAL<span>DEATH</span>
+          </h2>
+          <p>{data.deaths}</p>
+        </div>
+        <div className="boxdiv-boxes-box">
+          <h2>
+            TOTAL<span>ACTIVE</span>
+          </h2>
+          <p>{data.active}</p>
+        </div>
+        <div className="boxdiv-boxes-box">
+          <h2>
+            LAST<span>UPDATED</span>
+          </h2>
+          <p>{data.lastupdatedtime}</p>
+        </div>
+      </div>
+    </main>
+  );
+};
